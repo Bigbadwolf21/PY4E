d1046173ff15f28f431005756598cb6e68c724ee
Ayodele Ayomide <ayodeledprincipal@gmail.com>
2022-06-16 22:19:07 +0100

    Exercise 1

---
+++ variable_expression_statements/exercise1.py
@@ -0,0 +1,7 @@
+"""
+Exercise 2: Write a program that uses input to prompt a user for their
+name and then welcomes them.
+"""
+user_input = input("Please enter your name here\n")
+user_input = str(user_input)
+print("Welcome ", user_input)
